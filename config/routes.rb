a02a3764039b846f2900da0ffcde8343dc1dd452
Red <redkmonzon30@gmail.com>
2023-10-17 12:04:08 -0500

    Add about and main controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # Set default route to the main page 
+  root "main#index"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  
 end
