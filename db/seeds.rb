9f7bc4504a0d4a1e3c2307cf3dac3e087320c285
Red <redkmonzon30@gmail.com>
2023-10-17 19:22:40 -0500

    Add csv file and insert records to database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require "csv"
 
+filename = Rails.root.join("db/Spotify_Youtube.csv")
+
+csv_data = File.read(filename)
+
+artist = CSV.parse(csv_data, headers: true, encoding: "utf-8");
+
+Artist.delete_all
+Album.delete_all
+
+artist.each do |a|
+    mt_artist = Artist.find_or_create_by(name: a["Artist"])
+
+    unless mt_artist.valid?
+        artist = Artist.create(    
+            name: a["Artist"],
+            url_spotify: a["Url_spotify"]
+        )
+
+        album_type = AlbumType.create(type: a["Album_type"])
+        album = Album.create(name: a["Album"])
+    end
+end
+
+puts Artist.count
+puts AlbumType.count
+puts Album.count
