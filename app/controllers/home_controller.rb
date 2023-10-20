091c9e2f1271e706a643681d368bf9cfb16e064a
Red <redkmonzon30@gmail.com>
2023-10-20 00:51:24 -0500

    Add contents to the home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
   def index
+    @albums = Album.limit(10).order(name: :asc)
+
+    @artists = Artist.limit(10).order(name: :asc)
+
+    @songs = Song.limit(10).order(track_name: :asc)
   end
 end
