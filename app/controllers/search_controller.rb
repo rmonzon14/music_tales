40832cbe1c69de47d36bb902f3dea75e103b1c9a
Red <redkmonzon30@gmail.com>
2023-10-25 00:54:30 -0500

    Add conditional statements to the search page

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,12 +1,9 @@
 class SearchController < ApplicationController
   def index
     search_keyword = params[:q]
+    @search_model = params[:model]
     @found_songs = Song.where("track_name LIKE ?", "%#{search_keyword}%")
     @found_albums = Album.where("name LIKE ?", "%#{search_keyword}%");
     @found_artists = Album.where("name LIKE ?", "%#{search_keyword}%");
-  end
-
-  def show
-
   end
 end
--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,7 +1,7 @@
 class Artist < ApplicationRecord
     # associations
     has_many :albums
-    
+
     # validations
     validates :name, :url_spotify, presence: true
     validates :name, uniqueness: true
